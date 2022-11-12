ecf5eec6aaa0f4f69359cb659f1d028d68baf159
CORP\shperez <shaila.perez@gmail.com>
2022-11-12 16:16:10 +0100

    Thym

--- src/main/java/com/bahia/ejercicio/controller/MascotasController.java
+++ src/main/java/com/bahia/ejercicio/controller/MascotasController.java
@@ -2,27 +2,32 @@ package com.bahia.ejercicio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.bahia.ejercicio.dto.MascotaResponse;
 import com.bahia.ejercicio.dto.MascotasResponse;
 import com.bahia.ejercicio.service.MascotaService;
 
-@RestController
-@RequestMapping("/api")
-public class ApplicationController {
+@Controller
+public class MascotasController {
 
 	@Autowired
 	private MascotaService mascotaService;
 
-	
+
 	@GetMapping("/mascotas")
-	public ResponseEntity<MascotasResponse> getAllMascotas() {
-		return ResponseEntity.ok(mascotaService.getAllMascotas());
+	public String mostrarTablaMascotas(Model model) {
+		model.addAttribute("mascotas", mascotaService.getAllMascotas());
+		return "mascotas";
 	}
+	
+//	@GetMapping("/mascotas")
+//	public ResponseEntity<MascotasResponse> getAllMascotas() {
+//		return ResponseEntity.ok(mascotaService.getAllMascotas());
+//	}
 
 	@GetMapping("/mascota/{id}")
 	public ResponseEntity<MascotaResponse> getMascotasById(@PathVariable("id") Integer id) {
