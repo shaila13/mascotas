045f35f8c97a3978283219a9d25419dc88fd5530
CORP\shperez <shaila.perez@gmail.com>
2022-11-13 20:28:09 +0100

    M

--- src/main/java/com/bahia/ejercicio/controller/MascotasController.java
+++ src/main/java/com/bahia/ejercicio/controller/MascotasController.java
@@ -1,14 +1,14 @@
 package com.bahia.ejercicio.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.bahia.ejercicio.dto.MascotaResponse;
-import com.bahia.ejercicio.dto.MascotasResponse;
 import com.bahia.ejercicio.service.MascotaService;
 
 @Controller
@@ -20,8 +20,26 @@ public class MascotasController {
 
 	@GetMapping("/mascotas")
 	public String mostrarTablaMascotas(Model model) {
+		List<String> lista = new LinkedList<String>();
+		lista.add("INGRESADO");
+		lista.add("ALTA");
+		
+		model.addAttribute("estado", lista);
+		
 		model.addAttribute("mascotas", mascotaService.getAllMascotas());
 		return "mascotas";
+	}
+	
+	@GetMapping("/mascota/{id}")
+	public String getMascotasById(Model model,@PathVariable("id") Integer id) {
+		model.addAttribute("mascota", mascotaService.getMascotaById(id));
+		return "detalle";
+	}
+	
+	@GetMapping("/mascotas/{estado}")
+	public String getMascotasByEstado(Model model,@PathVariable("estado") String estado) {
+		model.addAttribute("mascotas", mascotaService.getMascotaByEstado(estado));
+		return "mascotasFiltrado";
 	}
 	
 //	@GetMapping("/mascotas")
@@ -29,15 +47,15 @@ public class MascotasController {
 //		return ResponseEntity.ok(mascotaService.getAllMascotas());
 //	}
 
-	@GetMapping("/mascota/{id}")
-	public ResponseEntity<MascotaResponse> getMascotasById(@PathVariable("id") Integer id) {
-		return ResponseEntity.ok(mascotaService.getMascotaById(id));
-	}
+//	@GetMapping("/mascota/{id}")
+//	public ResponseEntity<MascotaResponse> getMascotasById(@PathVariable("id") Integer id) {
+//		return ResponseEntity.ok(mascotaService.getMascotaById(id));
+//	}
 
-	@GetMapping("/mascotas/{estado}")
-	public ResponseEntity<MascotasResponse> getMascotasByEstado(@PathVariable("estado") String estado) {
-		return ResponseEntity.ok(mascotaService.getMascotaByEstado(estado));
-	}
+//	@GetMapping("/mascotas/{estado}")
+//	public ResponseEntity<MascotasResponse> getMascotasByEstado(@PathVariable("estado") String estado) {
+//		return ResponseEntity.ok(mascotaService.getMascotaByEstado(estado));
+//	}
 
 
 }
